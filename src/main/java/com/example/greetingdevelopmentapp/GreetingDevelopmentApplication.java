66a59f9aeedcd929c285150723003a41be2c7fc8
Roshni1998 <roshnimali847@gmail.com>
2021-08-16 10:30:51 +0530

    Added: Greeting App Development

---
+++ src/main/java/com/example/greetingdevelopmentapp/GreetingDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingdevelopmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingDevelopmentApplication.class, args);
+	}
+
+}
